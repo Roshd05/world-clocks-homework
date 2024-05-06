26af478899edb1b453055abf41f0d885da481ff1
Roshd05 <166666105+Roshd05@users.noreply.github.com>
2024-05-05 20:00:50 -0400

    fixed a mistake of calling a function twice in js

--- javascript/index.js
+++ javascript/index.js
@@ -1,37 +1,38 @@
 function updateTime() {
-let lisbonElement = document.querySelector("#lisbon");
-if (lisbonElement) {
-let lisbonDateElement = document.querySelector(".date");
-let lisbontimeElement = document.querySelector(".time");
-let lisbonTime = moment().tz("Europe/Lisbon");
+  let lisbonElement = document.querySelector("#lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = document.querySelector(".date");
+    let lisbontimeElement = document.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
 
-lisbonDateElement.innerHTML = lisbonTime.format("MMMM	Do YYYY");
-lisbontimeElement.innerHTML = lisbonTime.format( "h:mm:ss [<small>]A[</small>]"
-);
-}
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+    lisbontimeElement.innerHTML = lisbonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-function updateTime() {
-let sydneyElement = document.querySelector("#sydney");
-if (sydneyElement) {
-let sydneyDateElement = document.querySelector(".date");
-let sydneytimeElement = document.querySelector(".time");
-let sydneyTime = moment().tz("Australia/Sydney");
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = document.querySelector(".date");
+    let sydneytimeElement = document.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
 
-sydneyDateElement.innerHTML = sydneyime.format("MMMM	Do YYYY");
-sydneytimeElement.innerHTML = sydneyTimeTime.format( "h:mm:ss [<small>]A[</small>]"
-);
-}
+    sydneyDateElement.innerHTML = sydneyime.format("MMMM	Do YYYY");
+    sydneytimeElement.innerHTML = sydneyTimeTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
-    let cityTimeZone = event.target.value;
-    if (cityTimeZone === "current") {
-        cityTimeZone = moment.tz.guess();
-    }
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
     <div class="city">
     <div>
     <h2>${cityName}</h2>
